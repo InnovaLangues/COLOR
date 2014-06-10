d7f53de85a3581a0d490a8c7a1ed6cb80c12f835
mahmoud <charfeddine.mahmoud@gmail.com>
2014-06-10 16:03:42 +0200

    version the 10th june 2014

--- src/Innova/ForumMultimodalBundle/Controller/ContributionController.php
+++ src/Innova/ForumMultimodalBundle/Controller/ContributionController.php
@@ -10,6 +10,9 @@ use Innova\ForumMultimodalBundle\Form\TinymceForm;
 use Innova\ForumMultimodalBundle\Form\FileForm;
 use Innova\ForumMultimodalBundle\Entity\Contribution;
 use Innova\ForumMultimodalBundle\Entity\Subject;
+use \RecursiveIteratorIterator;
+use \RecursiveDirectoryIterator;
+use \ZipArchive;
 /**
  * Contribution Controller
  * @category   Controller
@@ -180,7 +183,7 @@ class ContributionController extends Controller
 	        // cette contribution et de type texte
 	        // la contribution de type oral est dans AjaxController
 	        // le dossier où on va mettre les fichiers uploadés
-	        $dir = $this->get('kernel')->getRootDir().'/../web/uploads/files/';
+	        $dir = $this->get('kernel')->getRootDir().'/../web/uploads/';
 	        $contributionEntity->setType("fichier");
 	        $pereoufils = $request->request->get('pereoufils');
 	        $contributionEntity->setFather($pereoufils);
@@ -202,7 +205,7 @@ class ContributionController extends Controller
 	            // on met le lien à null
 	            $contributionEntity->setLien("null");
 	        }
-	        $pathFile = "uploads/files/".$name;
+	        $pathFile = "uploads/".$name;
 	        // attribuer le nom de fichier à notre attribut de l'entité contribution
 	        $contributionEntity->setContents($pathFile);
 	        // debut : attribuer l'objet subject à notre attribut de l'entité contribution
@@ -336,7 +339,7 @@ class ContributionController extends Controller
         // on affecte l'objet user à la contribution
         $contributionEntity->setUser($user);
      	// le dossier où on va mettre les fichiers uploadés
-        $dir = $this->get('kernel')->getRootDir().'/../web/uploads/files/';
+        $dir = $this->get('kernel')->getRootDir().'/../web/uploads/';
         $contributionEntity->setType("fichier");
         $pereoufils = $request->request->get('pereoufils');
         $contributionEntity->setFather($pereoufils);
@@ -358,7 +361,7 @@ class ContributionController extends Controller
             // on met le lien à null
             $contributionEntity->setLien("null");
         }
-        $pathFile = "uploads/files/".$name;
+        $pathFile = "uploads/".$name;
         // attribuer le nom de fichier à notre attribut de l'entité contribution
         $contributionEntity->setContents($pathFile);
         // on met l'heure actuelle pour l'ajout d'une novelle contribution
@@ -403,7 +406,189 @@ class ContributionController extends Controller
         $listeContributions = $emContribution->findBy(array('subject' => $id));
     return $this->render('InnovaForumMultimodalBundle:Contribution:diaporama.html.twig', array('id' => $id,'date' => $date,'author' => $author,'listeContributions' => $listeContributions,'subject' => $subject,'consigne' => $consigne,));
   }
-  
+
+  /**
+   * [downloadSlideshowAction description]
+   * @param  Subject $subj [description]
+   * @return [type]        [description]
+   */
+  public function downloadSlideshowAction(Subject $subj)
+  {
+        //On récupère la requête
+        $request = $this->getRequest();
+        // l'id de sujet
+        $id = $subj->getId();
+        // la consigne donner avec le sujet
+        $consigne = $subj->getConsigne();
+        // l'auteur du sujet
+        $author = $subj->getAuteur();
+        // date d'ajout du sujet
+        $date = $subj->getDate();
+        // le sujet
+        $subject = $subj->getSujet();
+        // debut remplissage tableau $tableauContributionFather
+        $emContribution = $this->getDoctrine()
+                           ->getManager()
+                           ->getRepository('InnovaForumMultimodalBundle:Contribution');
+        $listeContributions = $emContribution->findBy(array('subject' => $id));
+        // supprimer les fichiers de dossier uploads
+        $folder = $this->get('kernel')->getRootDir().'/../web/diaporama/uploads/'; 
+        $dossier=opendir($folder);
+        while ($fichier = readdir($dossier))
+        {              
+          if ($fichier != "." && $fichier != ".." && !is_dir($folder.$fichier))
+          {
+                  //On selectionne le fichier et on le supprime
+                  $Vidage= $folder.$fichier;
+                  unlink($Vidage);
+          }
+        }
+        closedir($dossier);  
+         $text = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+                  <html xmlns="http://www.w3.org/1999/xhtml">
+                  <head>
+                    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.css" type="text/css" />
+                    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+                    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+                    <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css"/>
+                    <script src="js/jquery.js"></script>
+                    <script src="js/bootstrap.min.js"></script>
+                    <script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
+                    <script type="text/javascript" src="js/jquery.cycle.all.min.js"></script>
+                    <script type="text/javascript" src="js/slideshow.js"></script>
+                  </head>
+                  <body id="content" > 
+                    <div class="row">
+                      <div class="well">
+                        <button class="btn btn-warning btn-small" id="prevslide"><i class="icon-backward"></i></button>
+                        <button class="btn btn-warning btn-small" id="nextslide"><i class="icon-forward"></i></button>
+                        <button class="btn btn-warning btn-small" id="pauseslide"><i class="icon-pause"></i></button>
+                        <button class="btn btn-warning btn-small" id="playslide"><i class="icon-play"></i></button> 
+                      </div>
+                    </div>
+                    <br />
+                    <div class="row">
+                    <div class="span3">
+                    </div>
+                    <div class="span6"> 
+                      <ul id="slider" class="well">';
+                      $text .= '<li><p>Film - <strong><font color="red">'.$subject.'</font></strong></p><br /><p><font color="blue">'.$consigne.'</font></p><br /><p>Collectorama ajouter par '.$author.' le '.$date->format('d/m/Y').'</p></li>';
+        foreach($listeContributions as $cont)
+            {
+              if($cont->getType() == "oral")
+              {
+                $pathFile = $cont->getContents();
+                $source = $this->get('kernel')->getRootDir().'/../web/'.$pathFile;
+                $destination = $this->get('kernel')->getRootDir().'/../web/diaporama/'.$pathFile; 
+                copy($source, $destination); 
+                $text .= '<li><audio src="'.$pathFile.'" controls></audio></li>';
+              }
+              else if($cont->getType() == "fichier")
+              {
+                $pathFile = $cont->getContents();
+                $source = $this->get('kernel')->getRootDir().'/../web/'.$pathFile;
+                $destination = $this->get('kernel')->getRootDir().'/../web/diaporama/'.$pathFile; 
+                copy($source, $destination); 
+                if($cont->getExtension() == "png" || $cont->getExtension() == "jpg" || $cont->getExtension() == "jpeg" || $cont->getExtension() == "gif")
+                {
+                  $text .= '<li><img src="'.$pathFile.'" alt="" id="img" /></li> ';
+                }
+                else if($cont->getExtension() == "pdf")
+                {
+                  $text .= '<li><object id="cv" data="'.$pathFile.'" type="application/pdf"></object></li>';
+                }
+                else if($cont->getExtension() == "txt")
+                {
+                  $text .= '<li><object id="cv" data="'.$pathFile.'" type="text/plain"></object></li>';
+                }
+              }
+              else
+              {
+                $text .= '<li><p>'.$cont->getContents().'</p></li>';
+              }
+            }
+              $text .= '</ul>
+                        </div>
+                        <div class="span3">
+                        </div>
+                          
+                        </div></body>
+                        </html>
+                      ';
+              $filename = $this->get('kernel')->getRootDir().'/../web/diaporama/'.'index.html';
+              $open = fopen($filename, "w");
+              fwrite($open, $text); 
+              fclose($open); 
+
+                // commencer compression
+                $dirZip = $this->get('kernel')->getRootDir().'/../web/diaporama/';
+                $this->Zip($dirZip, './Diaporama.zip');
+                // proposer telechargement
+                header('Content-Transfer-Encoding: binary'); //Transfert en binaire (fichier).
+                header('Content-Disposition: attachment; filename="Diaporama.zip"'); //Nom du fichier.
+                header('Content-Length: '.filesize('Diaporama.zip')); //Taille du fichier.
+                readfile('Diaporama.zip');
+                // fin telechargement
+                // fin compression
+                // suppression zip
+                $fileZipToDelete = $this->get('kernel')->getRootDir().'/../web/Diaporama.zip';
+                unlink($fileZipToDelete);
+                
+
+   return $this->redirect($this->generateUrl('innova_forum_multimodal_view_slideshow', array('id' => $id)));
+  }
+  /**
+   * [Zip description]
+   * @param [type] $source      [description]
+   * @param [type] $destination [description]
+   */
+  function Zip($source, $destination)
+  {
+      if (is_string($source)) $source_arr = array($source); // convert it to array
+
+      if (!extension_loaded('zip')) {
+          return false;
+      }
+
+      $zip = new ZipArchive();
+      if (!$zip->open($destination, ZIPARCHIVE::CREATE)) {
+          return false;
+      }
+
+      foreach ($source_arr as $source)
+      {
+          if (!file_exists($source)) continue;
+          $source = str_replace('\\', '/', realpath($source));
+
+          if (is_dir($source) === true)
+          {
+              $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+              foreach ($files as $file)
+              {
+                  $file = str_replace('\\', '/', realpath($file));
+
+                  if (is_dir($file) === true)
+                  {
+                      $zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+                  }
+                  else if (is_file($file) === true)
+                  {
+                      $zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+                  }
+              }
+          }
+          else if (is_file($source) === true)
+          {
+              $zip->addFromString(basename($source), file_get_contents($source));
+          }
+
+      }
+
+      return $zip->close();
+
+  }
+
   /**
    * delete contribution and comments related thereto
    * 
